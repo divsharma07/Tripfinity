dcd1faa96df3af52d888421ee9e02be453f6bb73
divsharma07 <sharmadivyanshu1996@gmail.com>
2022-04-13 15:44:27 -0400

    fixed user model, added logout and other fixes

---
+++ app/src/main/java/com/app/tripfinity/utils/Constants.java
@@ -0,0 +1,9 @@
+package com.app.tripfinity.utils;
+
+public class Constants {
+    private Constants() {}
+    public static String FIREBASE_AUTH_TAG = "FirebaseAuthAppTag";
+    public static int RC_SIGN_IN = 123;
+    public static String USER = "user";
+    public static String USER_COLLECTION = "Users";
+}
--- app/src/main/java/com/app/tripfinity/view/MainActivity.java
+++ app/src/main/java/com/app/tripfinity/view/MainActivity.java
@@ -1,8 +1,14 @@
 package com.app.tripfinity.view;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Button;
 import android.widget.Toast;
 
 import com.app.tripfinity.R;
@@ -10,20 +16,21 @@ import com.app.tripfinity.model.User;
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
 import com.google.android.gms.auth.api.signin.GoogleSignInClient;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements FirebaseAuth.AuthStateListener {
     private GoogleSignInClient googleSignInClient;
+    private FirebaseAuth firebaseAuth = FirebaseAuth.getInstance();
     private final static String USER = "USER";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        User user = getUserFromIntent();
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
         initGoogleSignInClient();
-        Toast.makeText(this, "Logged in as user "+ user, Toast.LENGTH_LONG);
     }
-
 
     private User getUserFromIntent() {
         return (User) getIntent().getSerializableExtra(USER);
@@ -36,4 +43,57 @@ public class MainActivity extends AppCompatActivity {
         googleSignInClient = GoogleSignIn.getClient(this, googleSignInOptions);
     }
 
+    @Override
+    public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+        FirebaseUser firebaseUser = firebaseAuth.getCurrentUser();
+        if (firebaseUser == null) {
+            goToAuthInActivity();
+        }
+    }
+
+    private void goToAuthInActivity() {
+        Intent intent = new Intent(MainActivity.this, AuthActivity.class);
+        startActivity(intent);
+    }
+
+    private void signOut() {
+        singOutFirebase();
+        signOutGoogle();
+    }
+
+    private void singOutFirebase() {
+        firebaseAuth.signOut();
+    }
+
+    private void signOutGoogle() {
+        googleSignInClient.signOut();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        User user = getUserFromIntent();
+        firebaseAuth.addAuthStateListener(this);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        firebaseAuth.removeAuthStateListener(this);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem menuItem) {
+        if (menuItem.getItemId() == R.id.logout_button) {
+            signOut();
+            return true;
+        }
+        return super.onOptionsItemSelected(menuItem);
+    }
 }
