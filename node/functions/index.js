266ef71610c0784450bc1e29af203448f6544eee
Juhi <bhagtani.j@northeastern.edu>
2022-04-18 16:52:08 -0400

    added function for sending notification on invitation

--- node/functions/index.js
+++ node/functions/index.js
@@ -1,5 +1,7 @@
 "use strict";
 
 const sendemail = require("./sendemail");
+const invitenotification = require("./invitenotification");
 
 exports.sendemail = sendemail.sendemail;
+exports.invitenotification = invitenotification.invitenotification;
