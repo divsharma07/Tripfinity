1266eac600492a08b2b03f5b2d7a58d2c659de79
vibhore1997 <vibhore1997@gmail.com>
2022-04-19 00:15:54 -0400

    History expense recycler view without delete

--- app/src/main/java/com/app/tripfinity/Adapter/HistoryExpenseAdapter.java
+++ app/src/main/java/com/app/tripfinity/Adapter/HistoryExpenseAdapter.java
@@ -1,32 +1,63 @@
 package com.app.tripfinity.Adapter;
 
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.app.tripfinity.R;
+import com.app.tripfinity.model.Expense;
+
+import java.util.HashMap;
+import java.util.List;
+
 public class HistoryExpenseAdapter extends RecyclerView.Adapter<HistoryExpenseAdapter.HistoryExpenseViewHolder> {
+    private List<Expense> expenseList;
+    private HashMap<String, String> userEmailToName;
+
+    public HistoryExpenseAdapter(List<Expense> expenseList, HashMap<String, String> map) {
+        this.expenseList = expenseList;
+        userEmailToName = map;
+    }
+
     @NonNull
     @Override
     public HistoryExpenseAdapter.HistoryExpenseViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.history_expense_card, parent, false);
+        return new HistoryExpenseAdapter.HistoryExpenseViewHolder(view);
     }
 
     @Override
     public void onBindViewHolder(@NonNull HistoryExpenseAdapter.HistoryExpenseViewHolder holder, int position) {
+        Expense expense = expenseList.get(position);
 
+        holder.expenseName.setText(expense.getName());
+        holder.expenseAmount.setText(String.valueOf(expense.getAmount()));
+        holder.addedBy.setText(userEmailToName.get(expense.getAddedByUser()));
+        holder.memberCount.setText(String.valueOf(expense.getUserIds().size()));
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return expenseList.size();
     }
 
     public static class HistoryExpenseViewHolder extends RecyclerView.ViewHolder {
 
+        public TextView expenseName;
+        public TextView addedBy;
+        public TextView expenseAmount;
+        public TextView memberCount;
+
         public HistoryExpenseViewHolder(@NonNull View itemView) {
             super(itemView);
+            expenseName = itemView.findViewById(R.id.history_expense_name);
+            addedBy = itemView.findViewById(R.id.added_by);
+            expenseAmount = itemView.findViewById(R.id.expense_amount);
+            memberCount = itemView.findViewById(R.id.expense_member_count);
         }
     }
 }
--- app/src/main/java/com/app/tripfinity/view/HistoryExpenseActivity.java
+++ app/src/main/java/com/app/tripfinity/view/HistoryExpenseActivity.java
@@ -2,12 +2,15 @@ package com.app.tripfinity.view;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 
+import com.app.tripfinity.Adapter.ExpenseAdapter;
+import com.app.tripfinity.Adapter.HistoryExpenseAdapter;
 import com.app.tripfinity.R;
 import com.app.tripfinity.model.Expense;
 import com.app.tripfinity.viewmodel.HistoryExpenseViewModel;
@@ -18,6 +21,8 @@ import java.util.List;
 
 public class HistoryExpenseActivity extends AppCompatActivity {
     private RecyclerView historyRecyclerView;
+    private HistoryExpenseAdapter historyExpenseAdapter;
+    private RecyclerView.LayoutManager layoutManager;
     private List<Expense> expenseList;
     private HashMap<String, String> userEmailToName = new HashMap<>();
     private HistoryExpenseViewModel historyExpenseViewModel;
@@ -36,10 +41,19 @@ public class HistoryExpenseActivity extends AppCompatActivity {
         historyExpenseViewModel.getExpensesForTrip(tripId).observe(HistoryExpenseActivity.this, list -> {
             expenseList = list;
 
-
-
+            createHistoryRecyclerView(expenseList);
         });
 
+    }
+
+    private void createHistoryRecyclerView(List<Expense> expenseList) {
+        layoutManager = new LinearLayoutManager(this);
+        historyRecyclerView = findViewById(R.id.history_expense_recycler_view);
+        historyRecyclerView.setHasFixedSize(true);
+
+        historyExpenseAdapter = new HistoryExpenseAdapter(expenseList, userEmailToName);
+        historyRecyclerView.setAdapter(historyExpenseAdapter);
+        historyRecyclerView.setLayoutManager(layoutManager);
     }
 
     private void initHistoryExpenseViewModel() {
