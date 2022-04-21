43fa1a7c1e6abd7b7fd62daa474a8116026807f3
divsharma07 <sharmadivyanshu1996@gmail.com>
2022-04-21 00:25:48 -0400

    finalised trip engagement feature and added messaging notifications per token

--- node/functions/index.js
+++ node/functions/index.js
@@ -4,8 +4,10 @@ admin.initializeApp();
 const sendemail = require("./sendemail");
 const invitenotification = require("./invitenotification");
 const tripengagement = require("./tripengagement");
+const messagingnotification = require("./messagingnotification");
 
 exports.sendemail = sendemail.sendemail;
 exports.invitenotification = invitenotification.invitenotification;
 exports.tripengagement = tripengagement.tripengagement;
+exports.messagingnotification = messagingnotification.messagingnotification;
 
--- node/functions/tripengagement.js
+++ node/functions/tripengagement.js
@@ -1,32 +1,20 @@
 const functions = require("firebase-functions");
 const admin = require("firebase-admin");
-const { doc, query, collection, where, getDoc } = require("@firebase/firestore");
 const fcm = admin.messaging();
 
 exports.tripengagement = functions.firestore
     .document("Trips/{tripIdentifier}")
     .onCreate(async (snapshot, context) => {
-      const tripId = context.params.tripIdValue;
-      const trip = snapshot
-      const itinerarySnapshot = query(
-        collection(db, "Itinerary"),
-        where("trip", "==", tripIdentifier)
-      );
-
-      const itinerary = await getDoc(q);
-      const place = itinerary.places[0].city
-
-      // get the destination from the itinerary
-      const topic = trip.sourceLocation
-
-      // get the place from the user that created the trip which would be the topic this goes to
-
+      const trip = snapshot.data();
+      const destination = trip.destination;
+      const sourceLocation = trip.sourceLocation;
+      console.log("this is the sourceLocation" + snapshot);
       const payload = {
         notification: {
-          title: `Pack your bags and prepare for an adventure`,
-          body: `Users around you are traveling to ${place}`,
+          title: `Let's Go!!!`,
+          body: `Users around you are traveling to ${destination}. Pack your bags and prepare for an unforgattable expedition.`,
           clickAction: "RECEIVE_ACTIVITY",
         },
       };
-      fcm.sendToTopic(topic, payload);
+      fcm.sendToTopic(sourceLocation, payload);
     });
