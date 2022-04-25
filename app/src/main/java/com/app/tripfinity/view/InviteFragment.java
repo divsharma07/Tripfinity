47fd7bd82205b1a38e355128f952ef854c2505c1
Juhi <juhisbhagtani@gmail.com>
2022-04-25 01:00:52 -0400

    fix UI for invite fragment

--- app/src/main/java/com/app/tripfinity/view/InviteFragment.java
+++ app/src/main/java/com/app/tripfinity/view/InviteFragment.java
@@ -14,6 +14,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -75,7 +76,7 @@ public class InviteFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_invite, container, false);
-        Button inviteButton = view.findViewById(R.id.addUser);
+        ImageButton inviteButton = view.findViewById(R.id.addUser);
         text = view.findViewById(R.id.editTextInviteEmail);
         progressBar = view.findViewById(R.id.progressBar);
         users = new ArrayList<>();
